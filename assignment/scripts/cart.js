b15d855e0a49c4ee1b13b4dc09e58399a1c7d9f7
Curtis Kettler <curtiskettler@gmail.com>
2021-06-29 12:02:51 -0500

    listItem function created and tested

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,9 +2,11 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+//initial global variable
 const basket = [];
 console.log(`Items currently in basket: ${basket}`);
 
+//function to add items to the cart
 function addItem (item) {
   console.log(`${item} added to the basket!`);
   if (basket.push(item)) {
@@ -14,6 +16,17 @@ function addItem (item) {
   }
 } //end addItem
 
-addItem('milk');
-addItem('eggs');
+//testing addItem function
+console.log(addItem('milk'));
+console.log(addItem('eggs'));
 console.log(basket);
+
+//function to list out all items in the basket
+function listItems () {
+  for (let i=0; i < basket.length; i++){
+    console.log(basket[i]);
+  }
+} //end listItems
+
+//calling function for testing
+listItems();

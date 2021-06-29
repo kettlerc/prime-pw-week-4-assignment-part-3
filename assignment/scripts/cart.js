f528071ddfb1ed7b94ac048c1f3da19ab7be220f
Curtis Kettler <curtiskettler@gmail.com>
2021-06-29 11:42:53 -0500

    addItem function created and working

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,19 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+const basket = [];
+console.log(basket);
+
+function addItem (item) {
+  console.log('in addItem:');
+  if (basket.push(item)) {
+    return true;
+  } else {
+    return false;
+  }
+} //end addItem
+
+addItem('milk');
+console.log(addItem());
+console.log(basket);

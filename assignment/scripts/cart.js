bcdfa8855e9627ec2146435e217930af1d6cf28e
Curtis Kettler <curtiskettler@gmail.com>
2021-06-29 12:42:51 -0500

    addItem function updated to reference isFull

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -7,10 +7,13 @@ let basket = [];
 console.log('Your basket is currently empty');
 console.log(basket);
 
+//maximum items allowed in the basket
+const maxItems = 5;
+
 //function to add items to the cart
 function addItem (item) {
   console.log(`${item} added to the basket!`);
-  if (basket.push(item)) {
+  if (basket.push(item) && isFull(false)) {
     return true;
   } else {
     return false;
@@ -24,6 +27,12 @@ console.log(addItem('eggs'));
 console.log(basket);
 console.log(addItem('bread'));
 console.log(basket);
+console.log(addItem('cheese'));
+console.log(basket);
+console.log(addItem('steak'));
+console.log(basket);
+console.log(addItem('wine'));
+console.log(basket);
 
 //function to list out all items in the basket
 function listItems () {
@@ -36,19 +45,6 @@ function listItems () {
 //calling function for testing
 listItems();
 
-//function to reset the basket to an empty array
-function empty(){
-  basket = [];
-  console.log('Your basket is now empty');
-} //end empty
-
-//calling the function for testing
-empty();
-console.log(basket);
-
-//maximum items allowed in the basket
-const maxItems = 5;
-
 //function to check and see if the cart is full
 function isFull(){
   if (basket.length <= maxItems){
@@ -59,5 +55,15 @@ function isFull(){
 } // end isFull
 
 //testing to see if basket is full, currently empty because the empty function has been called
-//commented out empty and isFull functions properly
+//commented out empty to test and isFull functions properly
 console.log(isFull());
+
+//function to reset the basket to an empty array
+function empty(){
+  basket = [];
+  console.log('Your basket is now empty');
+} //end empty
+
+//calling the function for testing
+empty();
+// console.log(basket);
